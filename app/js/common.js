5d2b2ba4696ebfbcf022834024e46fc11a73a464
Anastasiia <na.konsta@gmail.com>
2018-10-25 18:58:57 +0300

    Pancakes widget was added

--- app/js/common.js
+++ app/js/common.js
@@ -4,15 +4,14 @@ $(function() {
     loop:true,
     margin:10,
     nav:true,
+    dots: false,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
     responsive:{
         0:{
-            items:1
-        },
-        600:{
             items:3
         },
-        1000:{
-            items:5
+        800:{
+            items:3
         }
     }
 });
