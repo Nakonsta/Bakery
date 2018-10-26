9a37031f563e388f1bf1bfae7c0f87b556112250
Anastasiia <na.konsta@gmail.com>
2018-10-26 18:14:57 +0300

    Third screen are ready, JQuery rating plugin is working

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,33 @@
 $(function() {
 
 	$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots: false,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    responsive:{
-        0:{
-            items:3
-        },
-        800:{
-            items:3
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:2
+            },
+            400:{
+                items:2
+            },
+            1200: {
+                items: 3
+            }
         }
-    }
-});
+    });
+
+    var configFontAwesome = {
+ custom: {
+     families: ['fontawesome'],
+     urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css']
+     },
+     fontactive: function () {
+         $('.rateit-fa').rateit();
+     }
+ };
+WebFont.load(configFontAwesome);
 
 });
