6238bb97c11fdf46399e6f7737c0c8cbea5f9d80
Anastasiia <na.konsta@gmail.com>
2018-11-07 19:00:44 +0300

    Slider was finished, ingridients are in developing

--- app/js/common.js
+++ app/js/common.js
@@ -19,15 +19,32 @@ $(function() {
         }
     });
 
+    $( '#my-slider' ).sliderPro({
+        width: 844,
+        height: 558,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        fade: false,
+        autoplay: true,
+        orientation: 'vertical',
+        thumbnailsPosition: 'right',
+        thumbnailWidth: 200,
+        thumbnailHeight: 186,
+        thumbnailPointer: true,
+        autoScaleLayers: false
+    });
+
+
     var configFontAwesome = {
- custom: {
-     families: ['fontawesome'],
-     urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css']
-     },
-     fontactive: function () {
-         $('.rateit-fa').rateit();
-     }
- };
-WebFont.load(configFontAwesome);
+     custom: {
+         families: ['fontawesome'],
+         urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css']
+         },
+         fontactive: function () {
+             $('.rateit-fa').rateit();
+         }
+     };
+    WebFont.load(configFontAwesome);
 
 });
